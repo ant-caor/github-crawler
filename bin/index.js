2d1795de9938e1cf9e016efa7454cf23dba082f0
ant-caor <contact@antoniocabezas.com>
2022-11-02 14:34:02 +0100

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
-import { getPRHeader, getPRComments, getPRIssueComments } from "./utils/gh.js";
+import {
+  getPRHeader,
+  getPRComments,
+  getPRIssueComments,
+  getPRS,
+} from "./utils/gh.js";
 import { savePRFile } from "./utils/fs.js";
 import { queryParams } from "./utils/prompt.js";
 
@@ -9,13 +14,30 @@ const getPRInfo = async (data) => {
   let issueComments;
 
   if (data.pr === "all") {
-    // TODO get all pr numbers
-    // TODO filter interesting prs
-    // TODO get interesting prs metadata
+    const headers = await getPRS(data);
+    headers.forEach(async (header) => {
+      comments = await getPRComments({
+        owner: data.owner,
+        repo: data.repo,
+        pr: header.number,
+      });
+      issueComments = await getPRIssueComments({
+        owner: data.owner,
+        repo: data.repo,
+        pr: header.number,
+      });
+      savePRFile(
+        { owner: data.owner, repo: data.repo, pr: header.number },
+        header,
+        comments,
+        issueComments
+      );
+    });
   } else {
     header = await getPRHeader(data);
     comments = await getPRComments(data);
     issueComments = await getPRIssueComments(data);
+    // TODO Sort nested comments by inReplyToId.
   }
 
   savePRFile(data, header, comments, issueComments);
