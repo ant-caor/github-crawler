50a02594a581f0c7cde9904d8817c2b93ff663f9
ant-caor <contact@antoniocabezas.com>
2022-11-11 13:17:17 +0100

    Update fs utils to allow us to save and read prs with sub dirs

--- bin/utils/fs.js
+++ bin/utils/fs.js
@@ -3,13 +3,20 @@ import chalk from "chalk";
 
 const log = console.log;
 
-const savePRFile = (data, header, comments, issueComments, reviews) => {
-  const file = `prs/${data.owner}/${data.repo}/${data.pr}.json`;
+const savePRFile = (
+  data,
+  header,
+  comments,
+  issueComments,
+  reviews,
+  dir = "prs"
+) => {
+  const file = `${dir}/${data.owner}/${data.repo}/${data.pr}.json`;
 
-  if (!fs.existsSync(`prs/${data.owner}/${data.repo}`)) {
-    fs.mkdirSync("prs");
-    fs.mkdirSync(`prs/${data.owner}`);
-    fs.mkdirSync(`prs/${data.owner}/${data.repo}`);
+  if (!fs.existsSync(`${dir}/${data.owner}/${data.repo}`)) {
+    fs.mkdirSync(`${dir}`);
+    fs.mkdirSync(`${dir}/${data.owner}`);
+    fs.mkdirSync(`${dir}/${data.owner}/${data.repo}`);
   }
 
   var writeStream = fs.createWriteStream(file.toLowerCase(), "utf8");
@@ -31,8 +38,8 @@ const savePRFile = (data, header, comments, issueComments, reviews) => {
   );
 };
 
-const readPRsDir = () => {
-  const dir = "prs";
+const readPRsDir = (dir = "prs") => {
+  const prs = [];
 
   fs.readdirSync(dir).forEach((owner) => {
     fs.readdirSync(`${dir}/${owner}`).forEach((repo) => {
@@ -41,7 +48,7 @@ const readPRsDir = () => {
           encoding: "utf-8",
         });
         const pr = JSON.parse(content, null, 2);
-
+        prs.push({ owner, repo, data: pr });
         console.log("Reading pr with number: ", pr.header.number);
         console.log("Reading pr with title: ", pr.header.title);
         console.log(`This pr has: ${pr.comments.length} comments`);
@@ -51,9 +58,7 @@ const readPRsDir = () => {
     });
   });
 
-  /*
-
-  */
+  return prs;
 };
 
 export { savePRFile, readPRsDir };
