ea7035dc143e8a0b8bb1c2072114c1a4941a2627
ant-caor <contact@antoniocabezas.com>
2022-11-11 12:50:45 +0100

    Update fs.js

--- bin/utils/fs.js
+++ bin/utils/fs.js
@@ -4,10 +4,12 @@ import chalk from "chalk";
 const log = console.log;
 
 const savePRFile = (data, header, comments, issueComments, reviews) => {
-  const file = `prs/${data.owner}_${data.repo}_${data.pr}.json`;
+  const file = `prs/${data.owner}/${data.repo}/${data.pr}.json`;
 
-  if (!fs.existsSync("prs")) {
+  if (!fs.existsSync(`prs/${data.owner}/${data.repo}`)) {
     fs.mkdirSync("prs");
+    fs.mkdirSync(`prs/${data.owner}`);
+    fs.mkdirSync(`prs/${data.owner}/${data.repo}`);
   }
 
   var writeStream = fs.createWriteStream(file.toLowerCase(), "utf8");
@@ -30,22 +32,28 @@ const savePRFile = (data, header, comments, issueComments, reviews) => {
 };
 
 const readPRsDir = () => {
-  console.log("Reading PRs dir...");
   const dir = "prs";
 
-  fs.readdir(dir, (err, files) => {
-    if (err) throw error;
+  fs.readdirSync(dir).forEach((owner) => {
+    fs.readdirSync(`${dir}/${owner}`).forEach((repo) => {
+      fs.readdirSync(`${dir}/${owner}/${repo}`).forEach((pr_file) => {
+        const content = fs.readFileSync(`${dir}/${owner}/${repo}/${pr_file}`, {
+          encoding: "utf-8",
+        });
+        const pr = JSON.parse(content, null, 2);
 
-    files.forEach((file) => {
-      const content = fs.readFileSync(`${dir}/${file}`, { encoding: "utf-8" });
-      const pr = JSON.parse(content, null, 2);
-
-      console.log("Reading pr with number: ", pr.header.number);
-      console.log("Reading pr with title: ", pr.header.title);
-      console.log(`This pr has: ${pr.comments.length} comments`);
-      console.log(`This pr has: ${pr.issueComments.length} issue comments`);
+        console.log("Reading pr with number: ", pr.header.number);
+        console.log("Reading pr with title: ", pr.header.title);
+        console.log(`This pr has: ${pr.comments.length} comments`);
+        console.log(`This pr has: ${pr.issueComments.length} issue comments`);
+        console.log(`This pr has: ${pr.reviews.length} reviews\n`);
+      });
     });
   });
+
+  /*
+
+  */
 };
 
 export { savePRFile, readPRsDir };
