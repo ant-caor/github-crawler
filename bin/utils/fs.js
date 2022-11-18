20abdaf8785ea05892fba143bbad453f5465d659
ant-caor <contact@antoniocabezas.com>
2022-11-18 12:21:32 +0100

    Update fs.js

--- bin/utils/fs.js
+++ bin/utils/fs.js
@@ -48,20 +48,29 @@ const readPRsDir = (dir = "prs") => {
   const prs = [];
 
   fs.readdirSync(dir).forEach((owner) => {
-    fs.readdirSync(`${dir}/${owner}`).forEach((repo) => {
-      fs.readdirSync(`${dir}/${owner}/${repo}`).forEach((pr_file) => {
-        const content = fs.readFileSync(`${dir}/${owner}/${repo}/${pr_file}`, {
-          encoding: "utf-8",
-        });
-        const pr = JSON.parse(content, null, 2);
-        prs.push({ owner, repo, data: pr });
-        console.log("Reading pr with number: ", pr.header.number);
-        console.log("Reading pr with title: ", pr.header.title);
-        console.log(`This pr has: ${pr.comments.length} comments`);
-        console.log(`This pr has: ${pr.issueComments.length} issue comments`);
-        console.log(`This pr has: ${pr.reviews.length} reviews\n`);
+    if (fs.lstatSync(`${dir}/${owner}`).isDirectory()) {
+      fs.readdirSync(`${dir}/${owner}`).forEach((repo) => {
+        if (fs.lstatSync(`${dir}/${owner}/${repo}`).isDirectory()) {
+          fs.readdirSync(`${dir}/${owner}/${repo}`).forEach((pr_file) => {
+            const content = fs.readFileSync(
+              `${dir}/${owner}/${repo}/${pr_file}`,
+              {
+                encoding: "utf-8",
+              }
+            );
+            const pr = JSON.parse(content, null, 2);
+            prs.push({ owner, repo, data: pr });
+            console.log("Reading pr with number: ", pr.header.number);
+            console.log("Reading pr with title: ", pr.header.title);
+            console.log(`This pr has: ${pr.comments.length} comments`);
+            console.log(
+              `This pr has: ${pr.issueComments.length} issue comments`
+            );
+            console.log(`This pr has: ${pr.reviews.length} reviews\n`);
+          });
+        }
       });
-    });
+    }
   });
 
   return prs;
